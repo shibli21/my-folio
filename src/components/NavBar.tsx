0cda5a844cb7db4e1ee186abb6cee4f9520386a2
shibli21 <syedshiblimahmud@gmail.com>
2020-10-07 12:54:28 +0600

    🎨 navlink style

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,6 @@
 import { Box, Flex, Link } from "@chakra-ui/core";
 import React from "react";
+import css from "../style/nav.scss";
 
 interface Props {}
 
@@ -7,9 +8,15 @@ const NavBar = (props: Props) => {
   return (
     <Box top={0} position="sticky" zIndex={10} bg="white">
       <Flex justifyContent="flex-end" fontSize="xl" py={10}>
-        <Link ml={8}>Contact</Link>
-        <Link ml={8}>About me</Link>
-        <Link ml={8}>Projects</Link>
+        <Box pos="relative" ml={8}>
+          <Link className={css.navigationLink}>Contact</Link>
+        </Box>
+        <Box pos="relative" ml={8}>
+          <Link className={css.navigationLink}>About me</Link>
+        </Box>
+        <Box pos="relative" ml={8}>
+          <Link className={css.navigationLink}>Projects</Link>
+        </Box>
       </Flex>
     </Box>
   );
