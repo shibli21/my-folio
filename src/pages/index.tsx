3149cd15bc5f4125ca8cdbb3dba6a4f83f570e76
shibli21 <syedshiblimahmud@gmail.com>
2020-10-07 19:24:39 +0600

    html head

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,15 @@ import { Footer } from "../components/Footer";
 import { Hero } from "../components/Hero";
 import NavBar from "../components/NavBar";
 import Projects from "../components/Projects";
+import Head from "next/head";
+import Logo from "../../public/logo.svg";
 
 const Index = () => (
   <>
+    <Head>
+      <title>shibli</title>
+      <link rel="icon" href={Logo} type="image/x-icon" />
+    </Head>
     <Container>
       <NavBar />
       <Hero />
