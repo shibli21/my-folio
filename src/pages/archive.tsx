d8ce8a299346c922b168d2a4eb21e365fa6b3758
shibli21 <syedshiblimahmud@gmail.com>
2020-10-09 19:09:25 +0600

    archive page

---
+++ src/pages/archive.tsx
@@ -0,0 +1,27 @@
+import { Text } from "@chakra-ui/core";
+import Head from "next/head";
+import React from "react";
+import Logo from "../../public/logo.svg";
+import { Container } from "../components/Container";
+
+interface Props {}
+
+const Archive = (props: Props) => {
+  return (
+    <>
+      <Head>
+        <title>archive</title>
+        <link rel="icon" href={Logo} type="image/x-icon" />
+      </Head>
+      <Container>
+        <Text fontSize="6xl" fontWeight="700">
+          Archive
+        </Text>
+        <Text fontSize="xl">A big list of things I’ve worked on</Text>
+        <></>
+      </Container>
+    </>
+  );
+};
+
+export default Archive;
