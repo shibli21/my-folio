19fb23901bb56a436d2aaed2b2b582b6a8a16bef
shibli21 <syedshiblimahmud@gmail.com>
2020-10-11 18:12:02 +0600

    remove preloader

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,14 @@
 import matter from "gray-matter";
 import { GetStaticProps } from "next";
 import Head from "next/head";
+import React from "react";
 import Logo from "../../public/logo.svg";
 import AboutMe from "../components/AboutMe";
 import { Container } from "../components/Container";
 import { Footer } from "../components/Footer";
 import { Hero } from "../components/Hero";
 import NavBar from "../components/NavBar";
-import { motion as Motion } from "framer-motion";
 import Projects from "../components/Projects";
-import { useRouter } from "next/router";
-import React, { useState, useEffect } from "react";
-import PreLoader from "../components/PreLoader";
 interface IndexProps {
   title: string;
   description: string;
@@ -23,17 +20,6 @@ const Index: React.FC<IndexProps> = ({
   title,
   description,
 }) => {
-  const [loading, setLoading] = useState(true);
-  const router = useRouter();
-
-  useEffect(() => {
-    window.addEventListener("load", () => {
-      setTimeout(() => {
-        setLoading(false);
-      }, 3000);
-    });
-  }, []);
-
   return (
     <>
       <Head>
@@ -43,22 +29,14 @@ const Index: React.FC<IndexProps> = ({
         <title>{title}</title>
         <link rel="icon" href={Logo} type="image/shibli-icon" />
       </Head>
-      {router.pathname === "/" && loading ? (
-        <Motion.div
-          transition={{ duration: 2, delay: 1 }}
-          animate={{ opacity: 0 }}
-        >
-          <PreLoader />
-        </Motion.div>
-      ) : (
-        <Container>
-          <NavBar />
-          <Hero />
-          <Projects data={featuredProjects} />
-          <AboutMe />
-          <Footer>Designed & Developed by Syed Shibli Mahmud</Footer>
-        </Container>
-      )}
+
+      <Container>
+        <NavBar />
+        <Hero />
+        <Projects data={featuredProjects} />
+        <AboutMe />
+        <Footer>Designed & Developed by Syed Shibli Mahmud</Footer>
+      </Container>
     </>
   );
 };
