46913ea07faa3e228b72c6a40280085cdf2f476e
shibli21 <syedshiblimahmud@gmail.com>
2020-10-12 15:31:24 +0600

    resume page

---
+++ src/pages/resume.tsx
@@ -0,0 +1,23 @@
+import { Text } from "@chakra-ui/core";
+import Head from "next/head";
+import React from "react";
+import Logo from "../../public/logo.svg";
+import { Container } from "../components/Container";
+
+const resume = () => {
+  return (
+    <>
+      <Head>
+        <title>shibli | resume</title>
+        <link rel="icon" href={Logo} type="image/x-icon" />
+      </Head>
+      <Container>
+        <Text fontSize="6xl" fontWeight="700">
+          Resume
+        </Text>
+      </Container>
+    </>
+  );
+};
+
+export default resume;
