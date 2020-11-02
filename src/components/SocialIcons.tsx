a1e3bd80140732626565d4742518a87ef93a4966
shibli21 <syedshiblimahmud@gmail.com>
2020-11-02 15:47:12 +0600

    style fixed

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -6,76 +6,79 @@ import css from "../style/index.scss";
 
 const SocialIcons = () => {
   const MotionBox = motion.custom(Box);
-  const [isLargerThanHD] = useMediaQuery([
-    "(min-width: 1280px)",
-    "(display-mode: browser)",
-  ]);
+  const [isLargerThanHD] = useMediaQuery(["(max-width: 1280px)"]);
 
   return (
-    <MotionBox
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1, transition: { delay: 1 } }}
-      mt={20}
-      className={css.verticalLine}
-      display={isLargerThanHD ? "block" : "none"}
-    >
-      <Link href="https://github.com/shibli21" isExternal aria-label="Github">
-        <Box
-          my={8}
-          _hover={{
-            transform: "scale(1.2)",
-            transitionDuration: ".2s",
-            color: "primary",
-          }}
-          as={FaGithub}
-          size="25px"
-        />
-      </Link>
-      <Link href="https://twitter.com/shibli21" isExternal aria-label="Twitter">
-        <Box
-          my={8}
-          _hover={{
-            transform: "scale(1.2)",
-            transitionDuration: ".2s",
-            color: "primary",
-          }}
-          size="25px"
-          as={FaTwitter}
-        />
-      </Link>
-      <Link
-        href="https://twitter.com/shibli21"
-        isExternal
-        aria-label="Instagram"
+    <Box display={isLargerThanHD ? "none" : "initial"}>
+      <MotionBox
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1, transition: { delay: 1 } }}
+        mt={20}
+        className={css.verticalLine}
+        display="block"
       >
-        <Box
-          my={8}
-          _hover={{
-            transform: "scale(1.2)",
-            transitionDuration: ".2s",
-            color: "primary",
-          }}
-          size="25px"
-          as={FaInstagram}
-        />
-      </Link>
-      <Link
-        href="https://www.linkedin.com/in/syed-shibli-mahmud-0bb357163/"
-        isExternal
-        aria-label="Linkedin"
-      >
-        <Box
-          my={8}
-          _hover={{
-            transform: "scale(1.2)",
-            transitionDuration: ".2s",
-            color: "primary",
-          }}
-          as={FaLinkedin}
-          size="25px"
-        />
-      </Link>
-    </MotionBox>
+        <Link href="https://github.com/shibli21" isExternal aria-label="Github">
+          <Box
+            my={8}
+            _hover={{
+              transform: "scale(1.2)",
+              transitionDuration: ".2s",
+              color: "primary",
+            }}
+            as={FaGithub}
+            size="25px"
+          />
+        </Link>
+        <Link
+          href="https://twitter.com/shibli21"
+          isExternal
+          aria-label="Twitter"
+        >
+          <Box
+            my={8}
+            _hover={{
+              transform: "scale(1.2)",
+              transitionDuration: ".2s",
+              color: "primary",
+            }}
+            size="25px"
+            as={FaTwitter}
+          />
+        </Link>
+        <Link
+          href="https://twitter.com/shibli21"
+          isExternal
+          aria-label="Instagram"
+        >
+          <Box
+            my={8}
+            _hover={{
+              transform: "scale(1.2)",
+              transitionDuration: ".2s",
+              color: "primary",
+            }}
+            size="25px"
+            as={FaInstagram}
+          />
+        </Link>
+        <Link
+          href="https://www.linkedin.com/in/syed-shibli-mahmud-0bb357163/"
+          isExternal
+          aria-label="Linkedin"
+        >
+          <Box
+            my={8}
+            _hover={{
+              transform: "scale(1.2)",
+              transitionDuration: ".2s",
+              color: "primary",
+            }}
+            as={FaLinkedin}
+            size="25px"
+          />
+        </Link>
+      </MotionBox>
+    </Box>
   );
 };
 
