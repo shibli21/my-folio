ad247746b5112016301d3ba1a1972ea7600e81d0
shibli21 <syedshiblimahmud@gmail.com>
2020-11-02 19:39:21 +0600

    social links style fixed

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -1,4 +1,4 @@
-import { Box, Link, useMediaQuery } from "@chakra-ui/core";
+import { Box, Link } from "@chakra-ui/core";
 import { motion } from "framer-motion";
 import React from "react";
 import { FaGithub, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
@@ -6,79 +6,71 @@ import css from "../style/index.scss";
 
 const SocialIcons = () => {
   const MotionBox = motion.custom(Box);
-  const [isLargerThanHD] = useMediaQuery(["(max-width: 1280px)"]);
 
   return (
-    <Box display={isLargerThanHD ? "none" : "initial"}>
-      <MotionBox
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1, transition: { delay: 1 } }}
-        mt={20}
-        className={css.verticalLine}
-        display="block"
+    <MotionBox
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1, transition: { delay: 1 } }}
+      className={css.verticalLine}
+    >
+      <Link href="https://github.com/shibli21" isExternal aria-label="Github">
+        <Box
+          mt={8}
+          mb={4}
+          _hover={{
+            transform: "scale(1.2)",
+            transitionDuration: ".2s",
+            color: "primary",
+          }}
+          as={FaGithub}
+          size="25px"
+        />
+      </Link>
+      <Link href="https://twitter.com/shibli21" isExternal aria-label="Twitter">
+        <Box
+          my={4}
+          _hover={{
+            transform: "scale(1.2)",
+            transitionDuration: ".2s",
+            color: "primary",
+          }}
+          size="25px"
+          as={FaTwitter}
+        />
+      </Link>
+      <Link
+        href="https://twitter.com/shibli21"
+        isExternal
+        aria-label="Instagram"
       >
-        <Link href="https://github.com/shibli21" isExternal aria-label="Github">
-          <Box
-            my={8}
-            _hover={{
-              transform: "scale(1.2)",
-              transitionDuration: ".2s",
-              color: "primary",
-            }}
-            as={FaGithub}
-            size="25px"
-          />
-        </Link>
-        <Link
-          href="https://twitter.com/shibli21"
-          isExternal
-          aria-label="Twitter"
-        >
-          <Box
-            my={8}
-            _hover={{
-              transform: "scale(1.2)",
-              transitionDuration: ".2s",
-              color: "primary",
-            }}
-            size="25px"
-            as={FaTwitter}
-          />
-        </Link>
-        <Link
-          href="https://twitter.com/shibli21"
-          isExternal
-          aria-label="Instagram"
-        >
-          <Box
-            my={8}
-            _hover={{
-              transform: "scale(1.2)",
-              transitionDuration: ".2s",
-              color: "primary",
-            }}
-            size="25px"
-            as={FaInstagram}
-          />
-        </Link>
-        <Link
-          href="https://www.linkedin.com/in/syed-shibli-mahmud-0bb357163/"
-          isExternal
-          aria-label="Linkedin"
-        >
-          <Box
-            my={8}
-            _hover={{
-              transform: "scale(1.2)",
-              transitionDuration: ".2s",
-              color: "primary",
-            }}
-            as={FaLinkedin}
-            size="25px"
-          />
-        </Link>
-      </MotionBox>
-    </Box>
+        <Box
+          my={4}
+          _hover={{
+            transform: "scale(1.2)",
+            transitionDuration: ".2s",
+            color: "primary",
+          }}
+          size="25px"
+          as={FaInstagram}
+        />
+      </Link>
+      <Link
+        href="https://www.linkedin.com/in/syed-shibli-mahmud-0bb357163/"
+        isExternal
+        aria-label="Linkedin"
+      >
+        <Box
+          my={4}
+          _hover={{
+            transform: "scale(1.2)",
+            transitionDuration: ".2s",
+            color: "primary",
+          }}
+          as={FaLinkedin}
+          size="25px"
+        />
+      </Link>
+    </MotionBox>
   );
 };
 
