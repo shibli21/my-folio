51c27af0290c511376ea9deb178b511c5284c6c7
shibli21 <syedshiblimahmud@gmail.com>
2020-12-14 22:08:46 +0600

    font link added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,7 +18,13 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;600;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <ColorModeScript initialColorMode="light" />
