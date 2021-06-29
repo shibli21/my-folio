06e5d91cf85a4e361c45e622b9c957c1907ffdef
Yuan1819 <syedshiblimahmud@gmail.com>
2021-06-29 13:02:07 +0600

    upgrade next version

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,13 +1,3 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-
-declare module "*.scss";
-declare module "*.svg";
-declare module "*.jpg" {
-  const image: string;
-  export default image;
-}
-declare module "*.png" {
-  const image: string;
-  export default image;
-}
+/// <reference types="next/image-types/global" />
