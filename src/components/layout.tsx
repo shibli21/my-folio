b4dd508fbfca70ebc1180e9824a449ff3fe591e3
shibli21 <syedshiblimahmud@gmail.com>
2022-04-28 04:12:30 +0600

    layout type

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,18 +1,14 @@
 import { Container } from "@chakra-ui/react";
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, PropsWithChildren } from "react";
 import { Footer } from "./Footer";
 import NavBar from "./NavBar";
 import SocialIcons from "./SocialIcons";
 
-/**
- * Layout which surounds every component
- */
+interface LayoutProps {}
 
-interface LayoutProps {
-  children: React.ReactNode;
-}
-
-const Layout: FunctionComponent<LayoutProps> = ({ children }) => {
+const Layout: FunctionComponent<PropsWithChildren<LayoutProps>> = ({
+  children,
+}) => {
   return (
     <>
       <NavBar />
