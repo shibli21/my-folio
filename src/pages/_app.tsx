19f249854029a3df1c79e70ec7d829aad4559dd6
shibli21 <syedshiblimahmud@gmail.com>
2022-05-07 21:23:16 +0600

    _app fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,25 +4,11 @@ import React, { useEffect, useState } from "react";
 import { theme } from "theme/theme";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  const [showing, setShowing] = useState(false);
-
-  useEffect(() => {
-    setShowing(true);
-  }, []);
-
-  if (!showing) {
-    return null;
-  }
-
-  if (typeof window === "undefined") {
-    return <></>;
-  } else {
-    return (
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    );
-  }
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 };
 
 export default MyApp;
